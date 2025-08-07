dbd88fc31827b1752873b2695042cc037a97d130
Júlia Guesser <159532759+guesserjuli4@users.noreply.github.com>
2025-08-07 15:58:26 -0300

    Update agendamentoonline.sql

--- agendamentoonline.sql
+++ agendamentoonline.sql
@@ -1,193 +1,83 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 07/08/2025 às 16:38
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+CREATE TABLE login (
+    idlogin INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(45) NOT NULL,
+    senha VARCHAR(45) NOT NULL
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE professoradmin (
+    cpf_professor INT PRIMARY KEY,
+    nome_professor VARCHAR(45) NOT NULL,
+    telefone_professor VARCHAR(45),
+    datanasc_professor VARCHAR(45),
+    especialidade VARCHAR(45),
+    disciplinaservico VARCHAR(45),
+    gmail_professor VARCHAR(45),
+    login_idlogin INT,
+    FOREIGN KEY (login_idlogin) REFERENCES login(idlogin)
+);
 
+CREATE TABLE disciplina (
+    iddisciplina INT AUTO_INCREMENT PRIMARY KEY,
+    disciplina VARCHAR(45),
+    professores VARCHAR(45)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE aluno (
+    RA INT PRIMARY KEY,
+    nome_aluno VARCHAR(45) NOT NULL,
+    gmail_aluno VARCHAR(45),
+    datanasc_aluno VARCHAR(45),
+    inicio_graduacao VARCHAR(45),
+    alunocel VARCHAR(45),
+    login_idlogin INT,
+    FOREIGN KEY (login_idlogin) REFERENCES login(idlogin)
+);
 
---
--- Banco de dados: `agendamentoonline`
---
+CREATE TABLE colaboradoradmin (
+    matricula INT PRIMARY KEY,
+    nome_colaborador VARCHAR(45),
+    gmail_colaborador VARCHAR(45),
+    datanasc_colaborador VARCHAR(45),
+    login_idlogin INT,
+    FOREIGN KEY (login_idlogin) REFERENCES login(idlogin)
+);
 
--- --------------------------------------------------------
+CREATE TABLE servicos (
+    idservicos INT PRIMARY KEY,
+    odontopedi VARCHAR(45),
+    endodontia VARCHAR(45),
+    periodontia VARCHAR(45),
+    avaliacao VARCHAR(45),
+    restauracao VARCHAR(45),
+    extracao VARCHAR(45),
+    proteseremovivel VARCHAR(45)
+);
 
---
--- Estrutura para tabela `agenda`
---
+CREATE TABLE paciente (
+    cpf_paciente INT PRIMARY KEY,
+    nome_paciente VARCHAR(45),
+    telefone_paciente VARCHAR(45),
+    datanasc_paciente VARCHAR(45),
+    gmail_paciente VARCHAR(45),
+    servico VARCHAR(45)
+);
 
-CREATE TABLE `agenda` (
-  `id_agenda` int(11) NOT NULL,
-  `nome_agenda` varchar(100) DEFAULT NULL,
-  `dia_semana` varchar(20) DEFAULT NULL,
-  `hora` time DEFAULT NULL,
-  `disciplina` varchar(100) DEFAULT NULL,
-  `professor_responsavel` varchar(100) DEFAULT NULL,
-  `paciente_agendado` varchar(100) DEFAULT NULL,
-  `telefone_paciente` varchar(20) DEFAULT NULL,
-  `aluno` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE agenda (
+    idagenda INT PRIMARY KEY,
+    nome_agenda VARCHAR(45),
+    diasemana VARCHAR(45),
+    hora VARCHAR(45),
+    disciplina VARCHAR(45),
+    professor_responsavel VARCHAR(45),
+    paciente_agendado VARCHAR(45),
+    telefone_paciente VARCHAR(45)
+);
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `aluno`
---
-
-CREATE TABLE `aluno` (
-  `ra` int(11) NOT NULL,
-  `nome_aluno` varchar(100) DEFAULT NULL,
-  `gmail_aluno` varchar(100) DEFAULT NULL,
-  `datanasc_aluno` date DEFAULT NULL,
-  `inicio_graduacao` date DEFAULT NULL,
-  `aluno_senha` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `colaboradoradmin`
---
-
-CREATE TABLE `colaboradoradmin` (
-  `matricula` int(11) NOT NULL,
-  `nome_colaborador` varchar(100) DEFAULT NULL,
-  `gmail_colaborador` varchar(100) DEFAULT NULL,
-  `datanasc_colaborador` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `disciplina`
---
-
-CREATE TABLE `disciplina` (
-  `id_disciplina` int(11) NOT NULL,
-  `disciplina` varchar(100) DEFAULT NULL,
-  `professor_responsavel` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `listaespera`
---
-
-CREATE TABLE `listaespera` (
-  `id_lista` int(11) NOT NULL,
-  `nome_paciente` varchar(100) DEFAULT NULL,
-  `telefone_paciente` varchar(20) DEFAULT NULL,
-  `servico` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `login`
---
-
-CREATE TABLE `login` (
-  `id_login` int(11) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `senha` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `paciente`
---
-
-CREATE TABLE `paciente` (
-  `cpf_paciente` varchar(14) NOT NULL,
-  `nome_paciente` varchar(100) DEFAULT NULL,
-  `telefone_paciente` varchar(20) DEFAULT NULL,
-  `datanasci_paciente` date DEFAULT NULL,
-  `gmail_paciente` varchar(100) DEFAULT NULL,
-  `servico_paciente` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `professoradmin`
---
-
-CREATE TABLE `professoradmin` (
-  `cpf_professor` int(45) NOT NULL,
-  `nome_professor` varchar(150) DEFAULT NULL,
-  `datanasci_professor` date DEFAULT NULL,
-  `especialidade` int(45) DEFAULT NULL,
-  `disciplinaexercida` int(11) DEFAULT NULL,
-  `gmail_professor` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `agenda`
---
-ALTER TABLE `agenda`
-  ADD PRIMARY KEY (`id_agenda`);
-
---
--- Índices de tabela `aluno`
---
-ALTER TABLE `aluno`
-  ADD PRIMARY KEY (`ra`);
-
---
--- Índices de tabela `colaboradoradmin`
---
-ALTER TABLE `colaboradoradmin`
-  ADD PRIMARY KEY (`matricula`);
-
---
--- Índices de tabela `disciplina`
---
-ALTER TABLE `disciplina`
-  ADD PRIMARY KEY (`id_disciplina`);
-
---
--- Índices de tabela `listaespera`
---
-ALTER TABLE `listaespera`
-  ADD PRIMARY KEY (`id_lista`);
-
---
--- Índices de tabela `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id_login`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Índices de tabela `paciente`
---
-ALTER TABLE `paciente`
-  ADD PRIMARY KEY (`cpf_paciente`);
-
---
--- Índices de tabela `professoradmin`
---
-ALTER TABLE `professoradmin`
-  ADD PRIMARY KEY (`cpf_professor`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE listaspera (
+    idlista INT PRIMARY KEY,
+    nomepaciente VARCHAR(45),
+    telefone VARCHAR(45),
+    servico VARCHAR(45),
+    colaboradoradmin_matricula INT,
+    FOREIGN KEY (colaboradoradmin_matricula) REFERENCES colaboradoradmin(matricula)
+);
